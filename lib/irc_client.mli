6e7b15cdedfcc05577239043da51079efd947848
John Else <john.else@citrix.com>
2013-04-02 19:45:57 +0100

    Add irc_client.mli
    Signed-off-by: John Else <john.else@citrix.com>

---
+++ lib/irc_client.mli
@@ -0,0 +1,17 @@
+module Make : functor (Io: Irc_transport.IO) ->
+  sig
+    type connection_t
+
+    val send_join : connection:connection_t -> channel:string -> unit Io.t
+    val send_nick : connection:connection_t -> nick:string -> unit Io.t
+    val send_pass : connection:connection_t -> password:string -> unit Io.t
+    val send_pong : connection:connection_t -> message:string -> unit Io.t
+    val send_privmsg : connection:connection_t ->
+      target:string -> message:string -> unit Io.t
+    val send_quit : connection:connection_t -> unit Io.t
+    val send_user : connection:connection_t ->
+      username:string -> mode:int -> realname:string -> unit Io.t
+
+    val connect : server:string -> port:int -> username:string -> mode:int ->
+      realname:string -> nick:string -> password:string -> connection_t Io.t
+  end
