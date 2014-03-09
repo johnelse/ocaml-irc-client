52f13e6f1b769f6179edd467bea04747e2ff15c1
John Else <john.else@citrix.com>
2014-03-09 22:49:17 +0000

    Add irc_helpers.mli
    Signed-off-by: John Else <john.else@citrix.com>

---
+++ lib/irc_helpers.mli
@@ -0,0 +1,7 @@
+val split : str:string -> c:char -> string list
+(** Split a string [str] at each occurence of the character [c]. *)
+
+val handle_input : buffer:Buffer.t -> input:string -> string list
+(** Given a [buffer] and a string [input], append the input to the buffer,
+ *  return all whole lines present in the buffer, and reinitalise the buffer to
+ *  contain only the substring which follows all the whole lines. *)
