eabe0ed676409bc278da04bb462fbfbddf40c2af
John Else <john.else@citrix.com>
2014-03-11 23:52:45 +0000

    Add doccomments to irc_client.mli
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/irc_client.mli
+++ lib/irc_client.mli
@@ -1,27 +1,43 @@
 module Make : functor (Io: Irc_transport.IO) ->
   sig
     type connection_t
+    (** A connection to an IRC server. *)
 
     val send_join : connection:connection_t -> channel:string -> unit Io.t
+    (** Send the JOIN command. *)
+
     val send_nick : connection:connection_t -> nick:string -> unit Io.t
+    (** Send the NICK command. *)
+
     val send_pass : connection:connection_t -> password:string -> unit Io.t
+    (** Send the PASS command. *)
+
     val send_pong : connection:connection_t -> message:string -> unit Io.t
+    (** Send the PONG command. *)
+
     val send_privmsg : connection:connection_t ->
       target:string -> message:string -> unit Io.t
+    (** Send the PRIVMSG command. *)
+
     val send_quit : connection:connection_t -> unit Io.t
+    (** Send the QUIT command. *)
+
     val send_user : connection:connection_t ->
       username:string -> mode:int -> realname:string -> unit Io.t
+    (** Send the USER command. *)
 
     val connect : addr:Io.inet_addr -> port:int -> username:string -> mode:int ->
       realname:string -> nick:string -> ?password:string -> unit ->
       connection_t Io.t
+    (** Connect to an IRC server at address [addr]. The PASS command will be
+     *  send if [password] is not None. *)
 
     val connect_by_name : server:string -> port:int -> username:string ->
       mode:int -> realname:string -> nick:string -> ?password:string -> unit ->
       connection_t option Io.t
-      (** Try to resolve the [server] name using DNS, otherwise behaves like
-          {!connect}. Returns [None] if no IP could be found for the given
-          name. *)
+    (** Try to resolve the [server] name using DNS, otherwise behaves like
+        {!connect}. Returns [None] if no IP could be found for the given
+        name. *)
 
     val listen : connection:connection_t ->
       callback:(
@@ -30,4 +46,7 @@ module Make : functor (Io: Irc_transport.IO) ->
         unit Io.t
       ) ->
       unit Io.t
+    (** [listen connection callback] listens for incoming messages on
+     *  [connection]. All server pings are handled internally; all other
+     *  messages are passed, along with [connection], to [callback]. *)
   end
