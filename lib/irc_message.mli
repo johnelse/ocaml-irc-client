8737811d84ba6f1da3aa5587c0b24c38094dc6a0
John Else <john.else@citrix.com>
2014-03-18 08:17:06 +0000

    Add doccomments to irc_message.mli
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/irc_message.mli
+++ lib/irc_message.mli
@@ -4,15 +4,21 @@ type t = {
   params: string list;
   trail: string option;
 }
+(** A type representing an IRC message. *)
 
 type parse_result =
   | Message of t
   | Parse_error of (string * string)
+(** A type representing an attempt to parse a string into an IRC message. *)
 
 val extract_prefix : string -> string option * string
+(** Exposed for testing - not intended for use. *)
 
 val extract_trail : string -> string * string option
+(** Exposed for testing - not intended for use. *)
 
 val extract_command_and_params : string -> string * string list
+(** Exposed for testing - not intended for use. *)
 
 val parse : string -> parse_result
+(** Attempt to parse an IRC message. *)
