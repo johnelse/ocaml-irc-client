e72114599d70039d02f6240d77c7d1f5d06a5a0f
John Else <john.else@gmail.com>
2017-03-31 22:22:57 +0100

    Fix docs
    Signed-off-by: John Else <john.else@gmail.com>

--- lib/irc_helpers.mli
+++ lib/irc_helpers.mli
@@ -1,11 +1,11 @@
 (** Helper functions for buffering data as it is read from a socket. *)
 
 val split : str:string -> c:char -> string list
-(** Split a string [str] at each occurrence of the character [c] *)
+(** Split a string [str] at each occurrence of the character [c]. *)
 
 val split1_exn : str:string -> c:char -> string * string
-(** Same as {!split1} but unsafe
-    @raise Not_found if the separator isn't found *)
+(** Split a string [str] at the first occurrence of the character [c].
+    @raise Not_found if the separator isn't found. *)
 
 val handle_input : buffer:Buffer.t -> input:string -> string list
 (** Given a [buffer] and a string [input], append the input to the buffer,
